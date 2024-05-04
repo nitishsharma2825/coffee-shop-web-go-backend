7ca5b0e3c35e89138ac9e2086bf4cddba1d5467e
nitishsharma2825 <sharnit2825@gmail.com.com>
2024-05-04 22:33:56 +0530

    added http server

--- main.go
+++ main.go
@@ -3,20 +3,41 @@ package main
 import (
 	"coffeeshop/coffee"
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	coffees, err := coffee.GetCoffees()
-	if err != nil {
-		fmt.Println("Error while getting coffeelist ", err)
-		return
-	}
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.html")
 
-	fmt.Println("Printing the list of coffees available in the CoffeeShop...")
-	for _, element := range coffees.List {
-		result := fmt.Sprintf("%s for $%v", element.Name, element.Price)
-		fmt.Println(result)
-	}
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to the Coffeeshop!",
+		})
+	})
+	r.GET("/home", coffeeList)
+	r.GET("/coffee", getCoffee)
 
-	fmt.Println("Is Latte Available? ", coffee.IsCoffeeAvailable("Latte"))
+	fmt.Println("Starting the server...")
+	r.Run(":8081")
+}
+
+func coffeeList(c *gin.Context) {
+	coffee, _ := coffee.GetCoffees()
+
+	// Call the HTML method of the Context to render a template
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"list": coffee.List,
+		},
+	)
+}
+
+func getCoffee(c *gin.Context) {
+	coffeelist, _ := coffee.GetCoffees()
+	c.String(http.StatusOK, " %s", coffeelist)
 }
